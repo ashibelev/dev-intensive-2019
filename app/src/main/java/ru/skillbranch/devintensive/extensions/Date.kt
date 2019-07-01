3153629f644f85662cb68210fc1855743e9d57c5
ashibelev <av.shibelev@gmail.com>
2019-07-01 15:11:16 +0300

    Humanize date

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -2,6 +2,7 @@ package ru.skillbranch.devintensive.extensions
 
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.absoluteValue
 
 /**
  * Created on 2019-06-30.
@@ -33,8 +34,73 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
     return this
 }
 
-fun Date.humanizeDiff(): String {
-    return ""
+val PLURAL_MINUTE = mapOf(
+    0 to "минута",
+    1 to "минуты",
+    2 to "минут",
+    3 to "минут"
+)
+
+val PLURAL_HOUR = mapOf(
+    0 to "час",
+    1 to "часа",
+    2 to "часов",
+    3 to "часов"
+)
+
+val PLURAL_DAY = mapOf(
+    0 to "день",
+    1 to "дня",
+    2 to "дней",
+    3 to "дней"
+)
+
+fun Date.humanizeDiff(date: Date = Date()): String {
+    val diff = (date.time - time)
+    val value = diff.absoluteValue
+
+    val text = when {
+        value <= 1 * SECOND -> return "только что"
+        value <= 45 * SECOND -> "несколько секунд"
+        value <= 75 * SECOND -> "минуту"
+        value <= 45 * MINUTE -> {
+            val minutes = value / MINUTE
+            "$minutes ${PLURAL_MINUTE[minutes.pluralize()]}"
+        }
+        value <= 75 * MINUTE -> "час"
+        value <= 22 * HOUR -> {
+            val hours = value / HOUR
+            "$hours ${PLURAL_HOUR[hours.pluralize()]}"
+        }
+        value <= 26 * HOUR -> "день"
+        value <= 360 * DAY -> {
+            val days = value / DAY
+            "$days ${PLURAL_DAY[days.pluralize()]}"
+        }
+        else -> return if (diff > 0) "более года назад" else "более чем через год"
+    }
+
+    return if (diff < 0) {
+        "через $text"
+    } else {
+        "$text назад"
+    }
+}
+
+// 20 - часов
+// 1 - час
+// 2-4 - часа
+// 5..часов
+fun Long.pluralize(): Int {
+    return if (this in 5..20) {
+        3
+    } else when (this % 10) {
+        0L -> 2
+        1L -> 0
+        in 2..5 -> 1
+        in 5..9 -> 2
+        else -> 3
+    }
 }
 
 enum class TimeUnits {
--- app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
+++ app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
@@ -2,8 +2,12 @@ package ru.skillbranch.devintensive
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import ru.skillbranch.devintensive.extensions.TimeUnits
+import ru.skillbranch.devintensive.extensions.add
+import ru.skillbranch.devintensive.extensions.humanizeDiff
 import ru.skillbranch.devintensive.models.User
 import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -47,6 +51,55 @@ class ExampleUnitTest {
     @Test
     fun transliterationTest() {
         println(Utils.transliteration("Женя Стереотипов "))
-        println(Utils.transliteration("Amazing Петр ","_"))
+        println(Utils.transliteration("Amazing Петр ", "_"))
+    }
+
+    @Test
+    fun humanizeDate() {
+        println(Date().humanizeDiff())
+
+        println(Date().add(15, TimeUnits.SECOND).humanizeDiff())
+        println(Date().add(-15, TimeUnits.SECOND).humanizeDiff())
+
+        println(Date().add(55, TimeUnits.SECOND).humanizeDiff())
+        println(Date().add(-65, TimeUnits.SECOND).humanizeDiff())
+
+        println(Date().add(2, TimeUnits.MINUTE).humanizeDiff())
+        println(Date().add(5, TimeUnits.MINUTE).humanizeDiff())
+        println(Date().add(11, TimeUnits.MINUTE).humanizeDiff())
+        println(Date().add(-2, TimeUnits.MINUTE).humanizeDiff())
+        println(Date().add(-5, TimeUnits.MINUTE).humanizeDiff())
+        println(Date().add(-11, TimeUnits.MINUTE).humanizeDiff())
+
+        println(Date().add(60, TimeUnits.MINUTE).humanizeDiff())
+        println(Date().add(-60, TimeUnits.MINUTE).humanizeDiff())
+
+        println(Date().add(2, TimeUnits.HOUR).humanizeDiff())
+        println(Date().add(5, TimeUnits.HOUR).humanizeDiff())
+        println(Date().add(21, TimeUnits.HOUR).humanizeDiff())
+        println(Date().add(-2, TimeUnits.HOUR).humanizeDiff())
+        println(Date().add(-5, TimeUnits.HOUR).humanizeDiff())
+        println(Date().add(-21, TimeUnits.HOUR).humanizeDiff())
+
+        println(Date().add(24, TimeUnits.HOUR).humanizeDiff())
+        println(Date().add(-24, TimeUnits.HOUR).humanizeDiff())
+
+        println(Date().add(2, TimeUnits.DAY).humanizeDiff())
+        println(Date().add(5, TimeUnits.DAY).humanizeDiff())
+        println(Date().add(21, TimeUnits.DAY).humanizeDiff())
+        println(Date().add(-2, TimeUnits.DAY).humanizeDiff())
+        println(Date().add(-5, TimeUnits.DAY).humanizeDiff())
+        println(Date().add(-21, TimeUnits.DAY).humanizeDiff())
+
+        println(Date().add(370, TimeUnits.DAY).humanizeDiff())
+        println(Date().add(-370, TimeUnits.DAY).humanizeDiff())
+
+        println(" --- ")
+        println(Date().add(-2, TimeUnits.HOUR).humanizeDiff())
+        println(Date().add(-5, TimeUnits.DAY).humanizeDiff())
+        println(Date().add(2, TimeUnits.MINUTE).humanizeDiff())
+        println(Date().add(7, TimeUnits.DAY).humanizeDiff())
+        println(Date().add(-400, TimeUnits.DAY).humanizeDiff())
+        println(Date().add(400, TimeUnits.DAY).humanizeDiff())
     }
 }
